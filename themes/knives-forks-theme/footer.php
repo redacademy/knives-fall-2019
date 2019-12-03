d1fa047e70167e8421ead6c69a122aa92c2fb675
Victor Lin <vlinmy06@gmail.com>
2019-12-03 15:52:00 -0800

    add placeholder text to footer

--- themes/knives-forks-theme/footer.php
+++ themes/knives-forks-theme/footer.php
@@ -12,11 +12,28 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
 	<img src="#" alt="footer image">
-	<!-- <div class="site-info">
-		<a href="<?php //echo esc_url('https://wordpress.org/'); 
-					?>"><?php //printf(esc_html('Proudly powered by %s'), 'WordPress'); 
-						?></a>
-	</div>.site-info -->
+	<div class="site-info">
+		<div>
+			<h4>our story</h4>
+			<p>We're a co-op</p>
+			<p>our investments</p>
+			<p>why we do it</p>
+			<p>our team</p>
+		</div>
+		<div>
+			<h4>how it works</h4>
+			<p>show me</p>
+		</div>
+		<div>
+			<h4>events</h4>
+			<p>apply to pitch</p>
+			<p>past events</p>
+		</div>
+		<div>
+			<h4>contact us</h4>
+		</div>
+	</div>
+	<!--.site-info -->
 	<a href="<?php bloginfo('url') ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/knives-forks-logo.svg" alt="logo"></a>
 	<p class="copyright">&copy; <span>knives and forks community investment co-operative 2015 harness capital </span>develops, manages and operates investment co-ops.</p>
 </footer><!-- #colophon -->

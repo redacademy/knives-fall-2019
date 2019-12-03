7e81dbe69ab21946adb49fcb29ad8648b428247e
Brooke <brooke.xiang@gmail.com>
2019-12-03 09:20:29 -0800

    updated gulpfile

--- themes/knives-forks-theme/gulpfile.js
+++ themes/knives-forks-theme/gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('browser-sync', function() {
   ];
 
   browserSync.init(files, {
-    proxy: 'localhost[:port-here]/[your-dir-name-here]'
+    proxy: 'http://localhost:8888/knivesforks/'
   });
 
   gulp.watch(files).on('change', browserSync.reload);

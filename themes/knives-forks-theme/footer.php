f86832104f92660e844d458aaed3f6db395b1449
Victor Lin <vlinmy06@gmail.com>
2019-12-03 15:15:28 -0800

    add footer logo & <p class="copyright"> to footer

--- themes/knives-forks-theme/footer.php
+++ themes/knives-forks-theme/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -7,16 +8,22 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<img src="#" alt="footer image">
+	<!-- <div class="site-info">
+		<a href="<?php //echo esc_url('https://wordpress.org/'); 
+					?>"><?php //printf(esc_html('Proudly powered by %s'), 'WordPress'); 
+						?></a>
+	</div>.site-info -->
+	<a href="<?php bloginfo('url') ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/knives-forks-logo.svg" alt="logo"></a>
+	<p class="copyright">&copy; <span>knives and forks community investment co-operative 2015 harness capital </span>develops, manages and operates investment co-ops.</p>
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
+
 </html>

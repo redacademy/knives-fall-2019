9f3cf4ea5fc9e2b1f0112b0f8bcf9e1d0b742f05
Victor Lin <vlinmy06@gmail.com>
2019-12-04 15:54:22 -0800

    remove work from js

--- themes/knives-forks-theme/js/script.js
+++
@@ -1,6 +0,0 @@
-// const mobile = document.getElementById('mobile-logo');
-
-// const desktop = document.getElementById('desktop-logo');
-// mobile.className = 'hide';
-
-// desktop.classList.remove('hide');

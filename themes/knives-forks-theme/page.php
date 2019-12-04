e75a77ffcbc3abdaf05ddf969c2e060c281d8850
Ariel <arielladret@gmail.com>
2019-12-03 16:33:14 -0800

    enqueue fa in functions; modify scss partials; add page-contact.php template-parts

--- themes/knives-forks-theme/page.php
+++ themes/knives-forks-theme/page.php
@@ -9,15 +9,22 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<?php
+				if(is_page('contact-us')){
+					while(have_posts()){
+						the_post();
+						get_template_part( 'template-parts/page','contact' );
+					}
+					
+				}
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+				else{
+					get_template_part( 'template-parts/content', 'page' ); 
+				}
+			?>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>

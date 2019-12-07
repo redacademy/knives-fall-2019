1cc3448bbae6ab9dfcf9f69a2d39519a455d28e1
Victor Lin <vlinmy06@gmail.com>
2019-12-06 21:56:04 -0800

    change variable to become global & add close menu

--- themes/knives-forks-theme/js/script.js
+++ themes/knives-forks-theme/js/script.js
@@ -1,8 +1,16 @@
 // click the hamburger menu
 const burger = document.getElementById('hamburger');
+
+const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+// closing menu 
+const cross = document.getElementById('cross');
+
 burger.addEventListener('click', function () {
-    const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
     mobileMenu.classList.remove('hide');
+});
+
+cross.addEventListener('click', function () {
+    mobileMenu.classList.add('hide');
 });
 // opens up & display menu items
 

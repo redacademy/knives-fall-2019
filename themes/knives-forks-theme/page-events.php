44819f5fe4fab596179dcda68f85d644948c9406
Ariel <arielladret@gmail.com>
2019-12-08 14:20:29 -0800

    add styles to event page, add pitch section to events page

--- themes/knives-forks-theme/page-events.php
+++ themes/knives-forks-theme/page-events.php
@@ -15,14 +15,14 @@ get_header(); ?>
 		// check if the repeater field has rows of data
 		if( have_rows('pitch_summary') ): ?>
 
-		<section class="flex_layout">
+		<section class="flex-layout">
 
 
 			<?php // loop through the rows of data
 			while ( have_rows('pitch_summary') ) : the_row(); ?>
 
-				<div class="flex_item">
-				<img src="<?php the_sub_field('image'); ?>"/>
+				<div class="flex-item-container">
+				<img alt="Summary Icons" src="<?php the_sub_field('image'); ?>"/>
 <h3 class="summary-title"><?php the_sub_field('title'); ?></h3>
 				<p class="summary-description"><?php the_sub_field('description'); ?></p>
 				

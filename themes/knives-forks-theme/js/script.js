b4dfc0ac837a027365dd4a04e2b56d66b17c4040
Brooke <brooke.xiang@gmail.com>
2019-12-09 12:45:54 -0800

    add styling to make pitch

--- themes/knives-forks-theme/js/script.js
+++ themes/knives-forks-theme/js/script.js
@@ -1,31 +1,25 @@
-(function ($) {
-    $(function () {
-        // when document is ready
-        // click the hamburger menu
-        const burger = $('#hamburger');
-
-        const mobileMenu = $('.mobile-menu');
-        // closing menu 
-        const cross = $('#cross');
-
-        burger.click(function () {
-            mobileMenu.removeClass('hide');
-        });
-
-        cross.click(function () {
-            mobileMenu.addClass('hide');
-        });
-        // opens up & display menu items
-
-        //
-
 (function($) {
-  //implement styling and element's position
-  $('.banner-content').css({
-    top: $('.banner').height() * 0.6
-  });
-  $('.entry-content  ul  li:last-child()').append($('.how-btn'));
-
-  // next button stylings for make-a-pitch
-  $('.gform_next_button').addClass('btn');
+  $(function() {
+    // when document is ready
+    // click the hamburger menu
+    const burger = $('#hamburger');
+    const mobileMenu = $('.mobile-menu');
+    // closing menu
+    const cross = $('#cross');
+    burger.click(function() {
+      mobileMenu.removeClass('hide');
+    });
+    cross.click(function() {
+      mobileMenu.addClass('hide');
+    });
+    // opens up & display menu items
+    //
+    //implement styling and element's position
+    $('.banner-content').css({
+      top: $('.banner').height() * 0.6
+    });
+    $('.entry-content  ul  li:last-child()').append($('.how-btn'));
+    // next button stylings for make-a-pitch
+    $('.gform_next_button').addClass('btn');
+  }); //when document is ready
 })(jQuery);

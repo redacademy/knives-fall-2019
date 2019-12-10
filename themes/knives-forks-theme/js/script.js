88e3d4434567b4a34f111cd2fd512292698d107f
Victor Lin <vlinmy06@gmail.com>
2019-12-09 20:50:55 -0800

    add const for #hidden-menu

--- themes/knives-forks-theme/js/script.js
+++ themes/knives-forks-theme/js/script.js
@@ -4,23 +4,32 @@
 
         const mobileMenu = $('.mobile-menu');
 
-        const cross = $('#cross');
+        const hiddenMenu = $('.hidden-menu');
 
-        const investor = $('.menu-item-64')
+        const cross = $('#cross');
+        const hidCross = $('#hid-cross');
+
+        const navHiddenMenu = $('#hidden-menu')
+        const investor = $('.menu-item-64');
+
         // when document is ready
         // click the hamburger menu
         // closing menu 
         // opens up & display menu items
         burger.click(function () {
             mobileMenu.removeClass('hide');
+            hiddenMenu.removeClass('hide');
         });
 
         cross.click(function () {
             mobileMenu.addClass('hide');
         });
+        hidCross.click(function () {
+            hiddenMenu.addClass('hide');
+        });
         // add .btn to menu item
         investor.addClass('btn');
-        //
+
 
 
 

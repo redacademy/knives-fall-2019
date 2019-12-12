f6d56b755a74106aa65bdeab77aa0986ce12d2e6
Servin <soservin@ymail.com>
2019-12-11 16:12:56 -0800

    applied effects for header nav menu

---
+++ themes/knives-forks-theme/page-contact.php
@@ -0,0 +1,68 @@
+<?php
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+	<?php while(have_posts()): the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'contact-wrapper' ); ?>>
+	<header class="entry-header-contact">
+
+
+
+
+	</header><!-- .entry-header -->
+
+
+	<div class="entry-content-contact">
+
+	<!-- //TO DO make a custom field to make it dynamic later// -->
+	
+	
+	<h2 class="contact-opportunity" >Looking for an opportunity? <br>
+	We’d love to hear from you </h2> 
+	<section class="contact-form-container"> 
+	<?php the_content(); ?>
+
+	<div class="media-enquiries"> 
+
+<h3> Media Enquiries: </h3> 
+<p> info@knivesandforks.ca </p>
+
+
+<h3> Find us on social media! </h3> 
+<div class="social-links">
+	<ul class="social-icons">
+		<li><i class="fab fa-twitter"></i></li>
+		<li><i class="fab fa-facebook-f"></i></li>
+		<li><i class="fab fa-linkedin-in"></i></li>
+	</ul> 				
+</div> 
+
+						</div><!-- .media-queries -->
+
+</section>
+
+
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
+
+
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
+<?php endwhile; ?>
+
+
+
+
+</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>

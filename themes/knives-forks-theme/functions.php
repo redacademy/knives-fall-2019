fe216d67a22b6c77a287c3323ced25546882ad41
Brooke <brooke.xiang@gmail.com>
2019-12-12 13:07:53 -0800

    add agreement form to become-investor

--- themes/knives-forks-theme/functions.php
+++ themes/knives-forks-theme/functions.php
@@ -150,6 +150,16 @@ function red_starter_scripts()
 	wp_enqueue_script('red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array('jquery'), '20151215', true);
 	wp_enqueue_style('red-starter-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css');
 
+		$localize = array(
+			'invest_download_file' => get_field('agreement_document', get_the_ID())		);
+		wp_localize_script( 'script', 'knivesforks_vars', $localize );
+
+		
+
+		
+
+	
+
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}

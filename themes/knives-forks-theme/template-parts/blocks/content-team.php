02751d64d01a883cbfadf19e431faca1d573343e
Victor Lin <vlinmy06@gmail.com>
2019-12-12 19:48:31 -0800

    add class team member

--- themes/knives-forks-theme/template-parts/blocks/content-team.php
+++ themes/knives-forks-theme/template-parts/blocks/content-team.php
@@ -12,6 +12,8 @@ $name = get_field('our_team');
 
 $role = get_field('organization_role');
 ?>
+<div class="team-member">
     <img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" />
     <p><?php echo $name ?></p>
     <p><?php echo $role ?></p>
+    </div>

08b412c71b3ab08a87a8b76cfa7a74980eeb65c7
Victor Lin <vlinmy06@gmail.com>
2019-12-13 13:28:21 -0800

    add hard code title

--- themes/knives-forks-theme/template-parts/blocks/content-team.php
+++ themes/knives-forks-theme/template-parts/blocks/content-team.php
@@ -9,11 +9,12 @@
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
 
-
-while (have_rows('our_team')) {
+?>
+<h2>Our Team</h2>
+<?php
+while (have_rows('team')) {
 
     the_row();
-    $heading = get_field('our_team');
 
     $name = get_sub_field('name');
 
@@ -24,7 +25,6 @@ while (have_rows('our_team')) {
     $bgColor = get_field('background_color'); ?>
 
     <section class="team-grid" style="background-color: <?php echo $bgColor; ?>">
-        <h2><?php echo $heading ?></h2>
         <div class="team-member">
             <img class="portrait" src="<?php echo $img['url']; ?>" alt="picture of <?php $name; ?>">
             <p class="name"><?php echo $name; ?></p>

cc5256f95a6e30ac20b53cdf8d2366e8234a06e5
Victor Lin <vlinmy06@gmail.com>
2019-12-13 14:38:05 -0800

    add h2 & background-color

--- themes/knives-forks-theme/template-parts/blocks/content-team.php
+++ themes/knives-forks-theme/template-parts/blocks/content-team.php
@@ -10,25 +10,30 @@
  */
 
 ?>
-<h2>Our Team</h2>
-<?php
-while (have_rows('team')) {
+<?php $bgColor = get_field('background_color'); ?>
+<?php $heading = get_field('heading'); ?>
+<section class="team" style="background-color:<?php echo $bgColor; ?>;">
+    <h2 class="our-team"><?php echo $heading ?></h2>
+    <div class="team-grid">
+        <?php
+        while (have_rows('team')) {
 
-    the_row();
+            the_row();
 
-    $name = get_sub_field('name');
+            $name = get_sub_field('name');
 
-    $img = get_sub_field('portrait');
+            $img = get_sub_field('portrait');
 
-    $role = get_sub_field('role');
+            $role = get_sub_field('role');
 
-    $bgColor = get_field('background_color'); ?>
-
-    <section class="team-grid" style="background-color: <?php echo $bgColor; ?>">
-        <div class="team-member">
-            <img class="portrait" src="<?php echo $img['url']; ?>" alt="picture of <?php $name; ?>">
-            <p class="name"><?php echo $name; ?></p>
-            <p class="role"><?php echo $role; ?></p>
-        </div>
-    </section>
-<?php } ?>
+            ?>
+            <div class="team-member">
+                <img class="portrait" src="<?php echo $img['url']; ?>" alt="picture of <?php $name; ?>">
+                <div class="desc">
+                <p class="name"><?php echo $name; ?></p>
+                <p class="role"><?php echo $role; ?></p>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</section>

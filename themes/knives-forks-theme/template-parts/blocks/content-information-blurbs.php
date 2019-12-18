add5960361b879bcae71c46b8cbc842fc76d322f
Victor Lin <vlinmy06@gmail.com>
2019-12-17 16:00:11 -0800

    move div to right place

--- themes/knives-forks-theme/template-parts/blocks/content-information-blurbs.php
+++ themes/knives-forks-theme/template-parts/blocks/content-information-blurbs.php
@@ -16,40 +16,41 @@
 ?>
 </pre> -->
 
-<div class="blurb-container">
 
+<?php
+$id = 'information-blurbs-' . $block['id'];
+if (!empty($block['anchor'])) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$className = 'information-blurbs';
+if (!empty($block['className'])) {
+    $className .= ' ' . $block['className'];
+}
+if (!empty($block['align'])) {
+    $className .= ' align' . $block['align'];
+}
+
+$members = get_field('become_a_member');
+$pitchs = get_field('make_a_pitch');
+$tmpCount = 1;
+// foreach($members as $member) {
+//     echo $member['title'];
+// }
+
+while (have_rows('become_a_member')) {
+    the_row(); ?>
     <?php
-    $id = 'information-blurbs-' . $block['id'];
-    if (!empty($block['anchor'])) {
-        $id = $block['anchor'];
-    }
+    $title = get_sub_field('title');
+    $subtitle = get_sub_field('subtitle');
+    $content = get_sub_field('content');
+    $image = get_sub_field('image');
+    $background_color = get_sub_field('background_color');
 
-    // Create class attribute allowing for custom "className" and "align" values.
-    $className = 'information-blurbs';
-    if (!empty($block['className'])) {
-        $className .= ' ' . $block['className'];
-    }
-    if (!empty($block['align'])) {
-        $className .= ' align' . $block['align'];
-    }
+    ?>
+    <div class="blurb-container">
 
-    $members = get_field('become_a_member');
-    $pitchs = get_field('make_a_pitch');
-    $tmpCount = 1;
-    // foreach($members as $member) {
-    //     echo $member['title'];
-    // }
-
-    while (have_rows('become_a_member')) {
-        the_row(); ?>
-        <?php
-        $title = get_sub_field('title');
-        $subtitle = get_sub_field('subtitle');
-        $content = get_sub_field('content');
-        $image = get_sub_field('image');
-        $background_color = get_sub_field('background_color');
-
-        ?>
         <section class="custom-copy-block image-copy" style="background-color: <?php echo $background_color ?>;">
             <div class="copy-block-content">
                 <article class="copy-block-text">
@@ -64,7 +65,7 @@
                 <img src="<?php echo $image['url']; ?>">
             </figure>
         </section>
-</div>
+    </div>
 <?php
                                                                             }
 

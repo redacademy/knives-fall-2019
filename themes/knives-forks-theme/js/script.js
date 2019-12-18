426bcdd5ae670f139ab6c17a4e2a7960b0f8f720
Ariel <arielladret@gmail.com>
2019-12-18 13:28:35 -0800

    fix btn style evnt + 404

--- themes/knives-forks-theme/js/script.js
+++ themes/knives-forks-theme/js/script.js
@@ -60,9 +60,9 @@
     });
 
     // button stylings
-    $('.gform_next_button').addClass('btn');
-    $('.gform_previous_button').addClass('btn');
-    $('.gform_button').addClass('btn');
+    $('.gform_next_button').addClass('btn btn-wide pitch');
+    $('.gform_previous_button').addClass('btn btn-wide pitch');
+    $('.gform_button').addClass('btn btn-wide pitch');
 
     // adding icon as ::before to each form page
     const gFormCurrentPage = parseInt(
